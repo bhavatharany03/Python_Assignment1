d8ae2a5b9b98dbbd354389e6faddf0a2948eab93
bhavatharany03 <166037679+bhavatharany03@users.noreply.github.com>
2024-08-13 19:19:04 +0530

    Update main.py

--- Q2/main.py
+++ Q2/main.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     sms = StudentManagementSystem()
 
     # Adding Students
-    sms.add_student("Ruthra","ruthravarshan@gmail.com")
-    sms.add_student("Ruthravarshan","ruthra@gmail.com")
-    sms.add_student("Varshan","varshan123@gmail.com")
+    sms.add_student("Parkavi","parkavi@gmail.com")
+    sms.add_student("Shraddha","shraddha@gmail.com")
+    sms.add_student("Iqbal","iqbal@gmail.com")
 
     # Adding Courses
     sms.add_course("Data Science")
@@ -15,18 +15,18 @@ if __name__ == "__main__":
 
 
     # Enrolling Students in Courses
-    sms.enroll_student_in_course("1", "1")  # Ruthra enrolls in Data Science
-    sms.enroll_student_in_course("2", "2")  # Ruthravarshan enrolls in Networking
-    sms.enroll_student_in_course("3","3")   # varshan enrolls in Data Structure
-    sms.enroll_student_in_course("2", "3")  # Ruthravarshan also enrolls in Data Structures
+    sms.enroll_student_in_course("1", "1")  
+    sms.enroll_student_in_course("2", "2")  
+    sms.enroll_student_in_course("3","3")   
+    sms.enroll_student_in_course("2", "3")  
 
     # Assigning Grades
-    sms.assign_grade("1", "1", 85)  # Assigns a grade of 85 to Ruthra in Data Science
-    sms.assign_grade("2", "2", 90)  # Assigns a grade of 90 to Ruthravarshanob in Networking
-    sms.assign_grade("3", "3", 84)  # Assigns a grade of 84 to varshan in Data Structure
+    sms.assign_grade("1", "1", 85)  
+    sms.assign_grade("2", "2", 90)  
+    sms.assign_grade("3", "3", 84)  
     sms.assign_grade("2", "3", 67)
 
     # Calculating GPA
-    sms.calculate_gpa("1")  # Calculates GPA for Ruthra
-    sms.calculate_gpa("2")  # Calculates GPA for Ruthravarshan
-    sms.calculate_gpa("3")  # Calculates GPA for varshan
+    sms.calculate_gpa("1")  
+    sms.calculate_gpa("2")  
+    sms.calculate_gpa("3")  
